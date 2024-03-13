378ee2e60dac06505dafbe903593073135e58bc2
Nathan <Nathan@PHOENIX>
2024-03-13 17:05:44 +0000

    Add project files.

---
+++ JuniorCodeTest/Models/RequestedUsersModel.cs
@@ -0,0 +1,18 @@
+﻿namespace JuniorCodeTest.Models
+{
+	public class RequestedUsersModel
+	{
+		public string? Title { get; set; }
+		public string? First { get; set; }
+		public string? Last { get; set; }
+		public int Age { get; set; }
+
+        public string? latitude { get; set; }
+        public string? longitude { get; set; }
+
+        
+
+
+
+    }
+}
