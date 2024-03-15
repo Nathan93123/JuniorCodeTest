43ac7ea2170346b3776de8dec2e514e9cfa9c41f
Nathan93123 <81781413+Nathan93123@users.noreply.github.com>
2024-03-15 11:54:36 +0000

    Update RandomUserApiService.cs
    Fixed and added correct logic

--- JuniorCodeTest/Services/RandomUserApiService.cs
+++ JuniorCodeTest/Services/RandomUserApiService.cs
@@ -1,7 +1,9 @@
-﻿using JuniorCodeTest.Interfaces;
+using JuniorCodeTest.Components;
+using JuniorCodeTest.Interfaces;
 using JuniorCodeTest.Models;
 
 using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace JuniorCodeTest.Services
 {
@@ -10,64 +12,56 @@ namespace JuniorCodeTest.Services
 		private const string randomUserEndPoint = "https://randomuser.me/api";
 
 		public async Task<List<RequestedUsersModel>> GetRandomUserDataFromApi()
+
+
+
+
 		{
 			var requiredDataList = new List<RequestedUsersModel>();
-			var response = await httpClient.GetAsync(randomUserEndPoint);
-
-			if (response.IsSuccessStatusCode)
+			while (requiredDataList.Count < 5)
 			{
-				string responseData = await response.Content.ReadAsStringAsync();
-				var randomUserModelData = JsonSerializer.Deserialize<UserModel>(responseData);
+				var response = await httpClient.GetAsync(randomUserEndPoint);
 
-				if (randomUserModelData != null)
+
+
+
+				if (response.IsSuccessStatusCode)
 				{
+					string responseData = await response.Content.ReadAsStringAsync();
+					var randomUserModelData = JsonSerializer.Deserialize<UserModel>(responseData);
 
-					foreach (var randomUser in randomUserModelData.results)
+					if (randomUserModelData != null)
 					{
-						var requiredData = new RequestedUsersModel()
-						{
-							Age = randomUser.dob.age,
-							First = randomUser.name.first,
-							Last = randomUser.name.last,
-							Title = randomUser.name.title,
-							Title = randomUser.name.title,
 
-						    Location = randomUser.location.coordinates
-						};
-
-						if (requiredDataList.Count >= 5)
+						foreach (var randomUser in randomUserModelData.results)
 						{
-							break;
+							var requiredData = new RequestedUsersModel()
+							{
+								Age = randomUser.dob.age,
+								First = randomUser.name.first,
+								Last = randomUser.name.last,
+								Title = randomUser.name.title,
+								Country = randomUser.location.country,
+								Latitude = randomUser.location.coordinates.longitude,
+								Longitude = randomUser.location.coordinates.latitude
+							};
+							requiredDataList.Add(requiredData);
+
+							
 						}
+					}
 
-                       
-                        requiredDataList.Add(requiredData);
-                        requiredDataList.Append(requiredData);
-
-                        requiredDataList.Add(requiredData);
-                        requiredDataList.Append(requiredData);
-
-                        requiredDataList.Add(requiredData);
-                        requiredDataList.Append(requiredData);
-
-                        requiredDataList.Add(requiredData);
-                        requiredDataList.Append(requiredData);
-
-                        requiredDataList.Add(requiredData);
-                        requiredDataList.Append(requiredData);
-
-
-                    }
+		
 
 				}
-
-				return requiredDataList;
+				else
+				{
+					throw new Exception("Failed to fetch data from the random user API");
+				}
 
 			}
-			else
-			{
-				throw new Exception("Failed to fetch data from the random user API");
-			}
+			return requiredDataList;
 		}
 	}
 }
+			
